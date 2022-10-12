d47469dbb8be7e62596e534de5a71585af382f58
asutosh04 <109894679+asutosh04@users.noreply.github.com>
2022-10-12 21:09:50 +0530

    Add files via upload

---
+++ LAB 2_27-07-22/Q2.cpp
@@ -0,0 +1,16 @@
+//2.WAP to check even or odd
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cout<<"Enter an integer number: ";
+	cin>>num;
+	
+	if(num%2==0)
+		cout<<num<<" is an EVEN number."<<endl;
+	else
+		cout<<num<<" is an ODD number."<<endl;
+	return 0;
+}

d8ea6f3bd84573b3a09421c0c17d8be4320c1c3a
B Asutosh <109894679+asutosh04@users.noreply.github.com>
2022-10-27 20:10:27 +0530

    Add files via upload

---
+++ Lab Assignment/LAB 7_19-10-22/Q4.cpp
@@ -0,0 +1,31 @@
+/*iv. WAP to demonstrate use of pure virtual function and abstract base class.*/
+
+#include <iostream>
+
+using namespace std;
+class Base // Abstract Class or Interface Class
+{
+public:
+    virtual void fun1() = 0; // Pure Virtual function
+    virtual void fun2() = 0;
+};
+class Derived : public Base
+{
+public:
+    void fun1()
+    {
+        cout << "fun1 of Derived" << endl;
+    }
+    void fun2()
+    {
+        cout << "fun2 of Derived" << endl;
+    }
+};
+int main()
+{
+    Base *b;
+    Derived d;
+    b = &d;
+    b->fun1();
+    b->fun2();
+}

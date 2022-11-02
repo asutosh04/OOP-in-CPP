548fb7fcd6c7c8f2a89e5b07ab4929d16c06805d
B Asutosh <109894679+asutosh04@users.noreply.github.com>
2022-11-02 10:20:03 +0530

    Add files via upload

---
+++ Lab Assignment/LAB 8_26-10-22/Q1.cpp
@@ -0,0 +1,43 @@
+/*To overload unary minus operator using member function*/
+
+#include <iostream>
+using namespace std;
+
+class Number
+{
+private:
+    int x, y, z;
+
+public:
+    Number() {}
+    Number(int a, int b, int c)
+    {
+        x = a;
+        y = b;
+        z = c;
+    }
+    void operator-() // member function
+    {
+        x = -x;
+        y = -y;
+        z = -z;
+    }
+    void display()
+    {
+        cout << "X:" << x << endl
+            << "Y:" << y << endl
+            << "Z:" << z << endl;
+    }
+};
+
+int main()
+{
+    cout << "\nBefore negative operator:\n";
+    Number n1(10, -20, 30);
+    n1.display();
+    cout << endl;
+    cout << "\nAfter negative operator:\n";
+    -n1;
+    n1.display();
+    return 0;
+}

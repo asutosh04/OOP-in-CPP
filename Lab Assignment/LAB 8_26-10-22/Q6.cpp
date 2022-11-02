97048df45107abe89bec3a1afaaf5e51113ddefe
B Asutosh <109894679+asutosh04@users.noreply.github.com>
2022-11-02 10:21:48 +0530

    Add files via upload

---
+++ Lab Assignment/LAB 8_26-10-22/Q6.cpp
@@ -0,0 +1,64 @@
+/*Addition of two matrix using operator overloading(m3=m1+m2)*/
+
+#include <iostream>
+using namespace std;
+
+class Matrix
+{
+    int a[3][3];
+
+public:
+    void input()
+    {
+        printf("\nEnter the numbers in matrix:\n");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                cin >> a[i][j];
+            }
+        }
+    }
+    Matrix operator+(Matrix m1)
+    {
+        Matrix t;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                t.a[i][j] = a[i][j] + m1.a[i][j];
+            }
+        }
+        return t;
+    }
+    void display()
+    {
+        cout<<"\nThe Matrix is:\n";
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                cout << a[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main()
+{
+    cout << "\nMatrix 1:\n";
+    Matrix m1;
+    m1.input();
+
+    cout << "\nMatrix 2:\n";
+    Matrix m2;
+    m2.input();
+
+    m1.display();
+    m2.display();
+    
+    cout << "\nThe sum of two matrix is:\n";
+    Matrix m3 = m1 + m2;
+    m3.display();
+}

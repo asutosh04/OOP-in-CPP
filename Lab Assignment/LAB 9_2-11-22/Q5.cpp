89f0eb61ceb4894a3bdd551faa40039b01c3b850
B Asutosh <109894679+asutosh04@users.noreply.github.com>
2022-11-06 00:34:24 +0530

    Add files via upload

---
+++ Lab Assignment/LAB 9_2-11-22/Q5.cpp
@@ -0,0 +1,36 @@
+/*Overload the insertion(<<) and extraction(>>) operators for accepting and displaying the time
+in hour,minute and second format*/
+
+#include <iostream>
+using namespace std;
+
+class Time
+{
+    int hour, min, sec;
+
+public:
+    friend istream& operator>>(istream &o,Time &t);
+    friend ostream& operator<<(ostream &o,Time &t);
+};
+
+istream& operator>>(istream &o,Time &t){
+    cout<<"\nEnter hour,minute,second : ";
+    o>>t.hour>>t.min>>t.sec;
+}
+
+ostream& operator<<(ostream &o,Time &t){
+    cout<<"\nHour : ";
+    o<<t.hour;
+    cout<<"\nMinute : ";
+    o<<t.min;
+    cout<<"\nSecond : ";
+    o<<t.sec;
+}
+
+int main()
+{
+    Time t1;
+    cin>>t1;
+    cout<<t1;
+    return 0;
+}
